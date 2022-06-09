a2644d4b1fefdb392d96ff5e9f28157b5467b87b
Matt Willy <matt.willy@gmail.com>
2022-06-09 07:16:39 -0600

    use on_trigger

--- airports/lib/airports.ex
+++ airports/lib/airports.ex
@@ -22,9 +22,15 @@ defmodule Airports do
     |> Flow.reject(&(&1.type == "closed"))
     |> Flow.partition(key: {:key, :country})
     |> Flow.group_by(& &1.country)
-    |> Flow.map(fn {country, data} -> {country, Enum.count(data)} end)
-    |> Flow.take_sort(10, fn {_, a}, {_, b} -> a > b end)
-    |> Enum.to_list()
-    |> List.flatten()
+    |> Flow.on_trigger(fn acc, _partition_info, {_type, _id, _trigger} ->
+      # Show progressin IEx or use the data for something else.
+      events =
+        acc
+        |> Enum.map(fn {country, data} -> {country, Enum.count(data)} end)
+
+      {events, acc}
+    end)
+    |> Enum.sort(fn {_, a}, {_, b} -> a > b end)
+    |> Enum.take(10)
   end
 end
